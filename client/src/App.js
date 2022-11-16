3e253bbd43c5b7235388e4fc57a070cabc0899ac
john.tg <john.tg177@gmail.com>
2022-11-16 15:32:20 +0530

    adding the search and load more design

--- client/src/App.js
+++ client/src/App.js
@@ -17,10 +17,24 @@ const App = () => {
     <div className="App">
       <h2>Photo Gallery</h2>
 
+      <div className="search-bar">
+        <input
+          className="search"
+          type="text"
+          placeholder="enter a search value.."
+        />
+        <button className="btn btn-search">Search</button>
+        <button className="btn btn-clear">Clear</button>
+      </div>
+
       <div className="images-grid">
         {imageList.map((img) => (
           <img src={img.url} alt={img.public_id} />
         ))}
+      </div>
+
+      <div className="load-more">
+        <button>Load more</button>
       </div>
     </div>
   );
